6f9721b2d88e8b3f03ed1a57d786dd744d64c993
lonalynn <@vscode>
2022-10-20 12:18:03 -0600

    this is unsanitary.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-project_6: project_6_.c
-	gcc -Wall Console_IO_Utility.c File_Input_Utility.c File_Output_Utility.c Arizona_Tax_Utility.c project_6_.c -o project_6 
+CC = gcc
+CFLAGS = -Wall -Wextra -pedantic -ggdb -fsanitize=address
+
+project_6: Console_IO_Utility.c File_Input_Utility.c File_Output_Utility.c Arizona_Tax_Utility.c project_6_.c
+	$(CC) $(CFLAGS) Console_IO_Utility.c File_Input_Utility.c File_Output_Utility.c Arizona_Tax_Utility.c project_6_.c -o project_6 
+
+clean:
+	/bin/rm -f project_6
--- project_6_.c
+++ project_6_.c
@@ -4,94 +4,99 @@
 #include "Arizona_Tax_Utility.h"
 #include "StandardConstants.h"
 
-// constants
-
 // function prototypes
 void createOutputString(char *outputString, int year, double income);
-void outputResultsToFile(const char *filename, const char * outputString);
+void outputResultsToFile(const char *filename, const char *outputString);
 
 int main()
-   {
-        
-     // TODO
+{
 
-     // Define Variables
-     char *fileName = NULL;
-     char *outputString = NULL; 
+    // Define Variables
+    char fileName[STD_STR_LEN] = {0};
+    char outputString[HUGE_STR_LEN] = {0};
 
-     int years; double income, totalIncome,
-     outputIncome; 
-     int line = 0;
+    // double income, totalIncome, outputIncome;
+    double income = 0;
+    double totalIncome = 0;
+    double outputIncome = 0;
+    int year;
+    int line = 0;
 
     // a pointer to the output file's name.
-     char *outputFilePtr = NULL;
-     
-     // Show Title
-        // functions: printString, printCharacters, printEndline(s)
-        printString("DUMMY TAX CALCULATOR V0.4");
-        printEndline();
-        printCharacters('=', 27);
-        printEndlines(2);
+    char outputFilePtr[HUGE_STR_LEN] = {0};
 
-     // Get input
-         // Get File name
-             // functions: promptForString, printEndline
-        promptForString("Enter file name containing income information: ", fileName);
-        printEndline();
+    // Show Title
+    // functions: printString, printCharacters, printEndline(s)
+    printString("DUMMY TAX CALCULATOR V0.4");
+    printEndline();
+    printCharacters('=', 27);
+    printEndlines(2);
 
-     // read file
-     // open file and check if it was successful
-        // function: fopen, print, if
-     if(openInputFile(fileName)) {
-            years = readIntegerFromFile();
-            while(!checkForEndOfInputFile()) {
-                if (line % 2 != 0) {
-                    income = readDoubleFromFile();
-                    totalIncome = totalIncome + income;
-                }
-                else {
-                    years = readIntegerFromFile();
-                }
-                outputIncome = calculateArizonaTax(totalIncome);
+    // Get input
+    // Get File name
+    // functions: promptForString, printEndline
+    promptForString("Enter file name containing income information: ", fileName);
+    printEndline();
 
-                // passing the pointer outputString, years, and the calculated income
-                createOutputString(outputString, years, outputIncome);
-
-                // creating output file name
-                sprintf(outputFilePtr, "Tax_Report_%d.txt", years);
-
-                // passing a pointer to the output file name, and a pointer to the outputString.
-                outputResultsToFile(outputFilePtr, outputString);
-
-                line = line + 1;
+    // read file
+    // open file and check if it was successful
+    // function: fopen, print, if
+    if (openInputFile(fileName))
+    {
+        year = readIntegerFromFile();
+        line = 1;
+        while (!checkForEndOfInputFile())
+        {
+            if (line % 2 == 0)
+            {
+                year = readIntegerFromFile();
+                line++;
+                continue;
             }
+            income = readDoubleFromFile();
 
-            printString("Arizona Tax for 8 years has been calculated.");
-            printEndline();
-            printString("The results can be viewed in the report files.");
-        } else {
-            printString("Error opening file.");
-            return 1;
+            totalIncome += income;
+            outputIncome = calculateArizonaTax(totalIncome);
+
+            createOutputString(outputString, year, outputIncome);
+            sprintf(outputFilePtr, "Tax_Report_%d.txt", year);
+            outputResultsToFile(outputFilePtr, outputString);
+
+            line++;
         }
 
-        // End program
-        printString("\nEnd Program");
-        return 0;
+        printString("Arizona Tax for 8 years has been calculated.");
+        printEndline();
+        printString("The results can be viewed in the report files.");
+        printEndline();
+    }
+    else
+    {
+        printString("Error opening file.");
+        printEndline();
+        return 1;
     }
 
-
+    // End program
+    printString("End Program");
+    printEndline();
+    return 0;
+}
 
 void createOutputString(char *outputString, int year, double income)
 {
-// this helper function prints (copies) an output string to outputString.
- sprintf(outputString, "For the tax year %d total AZ tax to be payed is %f $.", year, income);
+    // this helper function prints (copies) an output string to outputString.
+    sprintf(outputString, "For the tax year %d total AZ tax to be payed is $%.2f.", year, income);
 }
 
-void outputResultsToFile(const char * filename, const char *outputString)
+void outputResultsToFile(const char *filename, const char *outputString)
 {
-//open output file
- openOutputFile(filename);
+    // open output file
+    openOutputFile(filename);
 
-// display to outputfile
- writeStringToFile(outputString);
+    // display to outputfile
+    writeStringToFile(outputString);
+    writeEndlineToFile();
+
+    closeOutputFile();
 }
